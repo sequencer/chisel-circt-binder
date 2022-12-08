2c5ed5fff275ec6ea31b58369615d4bfc3dd5a6b
Jiuyang Liu <liu@jiuyang.me>
2022-12-08 15:00:27 +0000

    [test] add smoketest

---
+++ chisel-circt-binder/tests/src/Smoke.scala
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package tests.chisel3.circt
+
+import utest._
+import chisel3._
+
+object Smoke extends TestSuite {
+  val tests = Tests {
+    test("cigarette") {
+      class SmokeModule extends RawModule
+      Seq(
+        new chisel3.stage.phases.Elaborate,
+        chisel3.circt.Convert
+      ).foldLeft(firrtl.AnnotationSeq(Seq(
+        chisel3.stage.ChiselGeneratorAnnotation(() => new SmokeModule)
+      ))) { case (annos, phase) => phase.transform(annos) }
+    }
+  }
+
+}
